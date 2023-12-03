ec15d315b0c5b03bfb07817822f250afcc3408fd
Connie Leung <cleung334@gmail.com>
2023-12-03 23:03:15 +0800

    feat: stream webAssembly in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 
@@ -12,14 +12,39 @@ import { RouterOutlet } from '@angular/router';
   template: `
     <div class="container">
       <h2>Angular + WebAssembly Demo</h2>
+      <p>isPrime(11): {{ isPrimeResult() }}</p>
     </div>
   `,
   styles: [],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  webAssemblyExports!: Promise<WebAssembly.Exports>;
+  isPrimeResult = signal([]);
+
   constructor(title: Title) {
     title.setTitle('Ng WebAssembly Demo');
 
-    console.log('WebAssembly', WebAssembly);
+    // console.log('webAssemblyExports', this.webAssemblyExports);
+  }
+
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+
+    this.webAssemblyExports = WebAssembly.instantiateStreaming(fetch('../assets/release.wasm'), { env: {
+      abort: function(msg: string) {
+        throw new Error(msg || 'Abort called from wasm file');
+      },
+      "console.log": console.log
+    } })
+    .then((obj) => { 
+      console.log('obj', obj);
+      return obj.instance.exports;
+    });
+
+    this.webAssemblyExports.then((obj) => {
+      const results = (obj as any).findFirstNPrimes(5);
+      this.isPrimeResult.set(results);
+    })
   }
 }
